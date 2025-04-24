c915edff31ce3ee6049646deafb07f6915030396
JayAgra <69493224+JayAgra@users.noreply.github.com>
2025-04-24 14:34:29 -0700

    add empty project

---
+++ CA 403b/CA_403bApp.swift
@@ -0,0 +1,17 @@
+//
+//  CA_403bApp.swift
+//  CA 403b
+//
+//  Created by Jayen Agrawal on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct CA_403bApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
